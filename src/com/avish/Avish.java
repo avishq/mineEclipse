a1aabf8aeb6930457cc5358d4c1cbcc4b9d981df
avish <avish@avishs-mbp>
2017-01-06 02:42:07 +0530

    comitted

--- src/com/avish/Avish.java
+++ src/com/avish/Avish.java
@@ -3,6 +3,5 @@ package com.avish;
 public class Avish {
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		System.out.println("Abhhek");
 	}
 }
--- src/com/avish/Wine.java
+++ src/com/avish/Wine.java
@@ -2,6 +2,6 @@ package com.avish;
 
 public class Wine {
 	public static void main(String[] args) {
-		System.out.println("doinf hhhhhhhh great");
+		System.out.println("doing great");
 	}
 }
