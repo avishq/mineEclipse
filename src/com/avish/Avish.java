91d14812200dfb8a17782433b21ccf5398d5e133
avish <avish@avishs-mbp>
2017-01-06 02:38:09 +0530

    my changes

--- src/com/avish/Avish.java
+++ src/com/avish/Avish.java
@@ -3,6 +3,6 @@ package com.avish;
 public class Avish {
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		System.out.println("Abhishek");
+		System.out.println("Abhhek");
 	}
 }
